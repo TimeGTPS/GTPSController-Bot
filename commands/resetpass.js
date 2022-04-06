7ec200426167b2265fe4b4033a4b3f84d152be3b
Time <59316702+TimeTopia@users.noreply.github.com>
2022-04-06 17:10:22 +0800

    Update resetpass.js

--- commands/resetpass.js
+++ commands/resetpass.js
@@ -1,13 +1,12 @@
 const Discord = require('discord.js');
 const fs = require("fs");
 const config = require('./botconfig.json');
-const bcrypt = require("bcrypt");
 module.exports = {
     name: "resetpass",
     description: "The help command, what do you expect?",
 
     async run (client, message, args){
-        if (message.author.id !== '1231231231231' && message.author.id !== '933749403146125454') return message.reply("Sorry you can't do this command!")
+       // if (message.author.id !== '1231231231231' && message.author.id !== '933749403146125454') return message.reply("Sorry you can't do this command!")
         const user = args[0]
         const pass = args[1]
       if(args[0] == null)
